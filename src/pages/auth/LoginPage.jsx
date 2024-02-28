59dea3ef03a8d7ca2a22774bb0ce677095ea5e83
Anastasia1ukina <lukina02anastasiy@gmail.com>
2024-02-28 19:40:29 +0300

    feat

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -11,7 +11,6 @@ import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useForm } from 'react-hook-form';
@@ -83,7 +82,6 @@ export const LoginPage = () => {
                             }}
                         >
                             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                                {/* <LockOutlinedIcon /> */}
                             </Avatar>
                             <Typography component="h1">
                                 Sign in
@@ -126,7 +124,6 @@ export const LoginPage = () => {
                                                 <IconButton
                                                     aria-label="toggle password visibility"
                                                     onClick={handleClickShowPassword}
-                                                    // onMouseDown={handleMouseDownPassword}
                                                     edge="end"
                                                 >
                                                     {showPassword ? <VisibilityOff /> : <Visibility />}
